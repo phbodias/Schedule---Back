642d728f74824afca0f6ce31d2fb544c94847307
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-03 20:10:35 -0300

    :bricks: Function to encrypt password

---
+++ src/utils/encrypt.ts
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+
+export async function encryptPassword(password: string): Promise<string> {
+  const saltRound = Number(process.env.ENCRYPT_SALTS) || 20;
+  const salt = await bcrypt.genSalt(saltRound);
+  const crypt = await bcrypt.hash(password, salt);
+
+  return crypt;
+}
