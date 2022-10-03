83aee38a5fca66bd087d94ae91555b2b9572c9da
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-03 20:12:40 -0300

    :bricks: Services to create user

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,6 +1,16 @@
-import initializedRepository from "../repositories/userRepository";
+import * as userRepository from "../repositories/userRepository";
+import { TUserBody } from "../types/userTypes";
+import { encryptPassword } from "../utils/encrypt";
 
-export default async function initializedService() {
-  const data = await initializedRepository();
-  return data;
+export async function create(user: TUserBody) {
+  const emailNotAvailable: boolean = !!(await findByEmail(user.email));
+  if (emailNotAvailable)
+    throw { code: "Conflict", message: "Email already in use" };
+
+  user.password = await encryptPassword(user.password);
+  return await userRepository.create(user);
+}
+
+export async function findByEmail(email: string) {
+  return await userRepository.findByEmail(email);
 }
