eadd712f1370465c3d9193b68abfe552ac21006b
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-03 19:46:38 -0300

    :bricks: Insert new user in db

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,3 +1,6 @@
-export default async function initializedRepository() {
-  return ["Server in running"];
+import prisma from "../databases/postgres";
+import { TUserBody } from "../types/userTypes";
+
+export default async function create(user: TUserBody) {
+  return prisma.users.create({ data: user });
 }
