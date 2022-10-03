141ea152791b836c616b23ff0e311d0ec9382631
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-03 19:51:42 -0300

    :bricks: Find user by email

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,6 +1,12 @@
+import { Users } from "@prisma/client";
 import prisma from "../databases/postgres";
 import { TUserBody } from "../types/userTypes";
 
-export default async function create(user: TUserBody) {
+export async function create(user: TUserBody) {
   return prisma.users.create({ data: user });
+}
+
+export async function findByEmail(email: string) {
+  const user: Users = await prisma.users.findFirst({ where: { email } });
+  return user;
 }
