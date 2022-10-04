f198907285b0d05442f31c86e59499542c4a5e08
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-04 00:11:06 -0300

    :ok_hand: perf: Max length to passwords

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -2,11 +2,11 @@ import joi from "joi";
 
 export const signupSchema = joi.object({
   name: joi.string().min(3).required(),
-  password: joi.string().min(6).required(),
+  password: joi.string().min(6).max(30).required(),
   email: joi.string().email().required(),
 });
 
 export const signinSchema = joi.object({
-  password: joi.string().min(6).required(),
+  password: joi.string().min(6).max(30).required(),
   email: joi.string().email().required(),
 });
