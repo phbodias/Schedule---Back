9bff69396a09e0d4c5de31a3ecade9be6debcfd5
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-03 21:07:33 -0300

    :bricks: Find user by id

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -6,6 +6,11 @@ export async function create(user: TUserBody) {
   return prisma.users.create({ data: user });
 }
 
+export async function findById(id: number): Promise<Users> {
+  const user: Users = await prisma.users.findFirst({ where: { id } });
+  return user;
+}
+
 export async function findByEmail(email: string): Promise<Users> {
   const user: Users = await prisma.users.findFirst({ where: { email } });
   return user;
