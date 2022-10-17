0cac5951c5d42cc49fdb38c069b0f5d7d5b5371d
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-17 19:10:27 -0300

    :white_check_mark: test: sign-up tests

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -1,28 +1,27 @@
 import supertest from "supertest";
-import app from "../src/index";
+import app from "../src/app";
 import { userFactory } from "./factories/userFactories";
-import prisma from "../src/database/database";
+import prisma from "../src/databases/postgres";
 
 beforeEach(async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE "Users";`;
+  await prisma.$executeRaw`TRUNCATE TABLE "users" CASCADE;`;
 });
 
 afterAll(async () => {
   await prisma.$disconnect();
 });
 
-describe("Testing POST /signup", () => {
+describe("Testing POST /sign-up", () => {
   it("Insert a new user and return status code 201", async () => {
     const body = await userFactory();
-    const result = await supertest(app).post("/signup").send(body);
+    const result = await supertest(app).post("/sign-up").send(body);
     expect(result.status).toBe(201);
-    expect(result.body.createdAt.length).toBeGreaterThan(0);
   });
 
   it("Tries to insert user with email already registered in the database and receives status 409", async () => {
     const body = await userFactory();
-    await supertest(app).post("/signup").send(body);
-    const result = await supertest(app).post("/signup").send(body);
+    await supertest(app).post("/sign-up").send(body);
+    const result = await supertest(app).post("/sign-up").send(body);
     expect(result.status).toBe(409);
   });
 
@@ -30,72 +29,9 @@ describe("Testing POST /signup", () => {
     const body = {
       email: "agoogle.com",
       password: "aaaaaaaaaaaaaa",
-      confirmPassword: "aaaaaaaaaaaaaa",
     };
 
-    const result = await supertest(app).post("/signup").send(body);
-    expect(result.status).toBe(422);
-  });
-
-  it("Tries to insert user without password and receives status 422", async () => {
-    const body = {
-      email: "a@gmail.com",
-      password: "",
-      confirmPassword: "",
-    };
-
-    const result = await supertest(app).post("/signup").send(body);
-    expect(result.status).toBe(422);
-  });
-
-  it("Tries to insert user with confirmPassword different than password and receives status 422", async () => {
-    const body = {
-      email: "a@gmail.com",
-      password: "aaaaaaaaaaaaaa",
-      confirmPassword: "bbbbbbbbbbbb",
-    };
-
-    const result = await supertest(app).post("/signup").send(body);
-    expect(result.status).toBe(422);
-  });
-});
-
-describe("Testing POST /signin", () => {
-  it("Login with a correct body and return status code 200 and a token", async () => {
-    const body = await userFactory();
-    await supertest(app).post("/signup").send(body);
-    const user = { email: body.email, password: body.password };
-    const result = await supertest(app).post("/signin").send(user);
-    expect(result.status).toBe(200);
-    expect(result.body.token.length).toBeGreaterThan(0);
-  });
-
-  it("Login with a incorrect password and return status code 401", async () => {
-    const body = await userFactory();
-    await supertest(app).post("/signup").send(body);
-    const user = {
-      email: body.email,
-      password: body.password + "incorrect_pass",
-    };
-    const result = await supertest(app).post("/signin").send(user);
-    expect(result.status).toBe(401);
-  });
-
-  it("Login with a email without register and return status code 401", async () => {
-    const body = await userFactory();
-    const user = { email: body.email, password: body.password };
-    const result = await supertest(app).post("/signin").send(user);
-    expect(result.status).toBe(404);
-  });
-
-  it("Login with a incorrect body and return status code 422", async () => {
-    const body = await userFactory();
-    await supertest(app).post("/signup").send(body);
-    const user = {
-      email: "invalid_email.com",
-      password: body.password + "incorrect_pass",
-    };
-    const result = await supertest(app).post("/signin").send(user);
+    const result = await supertest(app).post("/sign-up").send(body);
     expect(result.status).toBe(422);
   });
 });
