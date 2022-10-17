34c3392468a8767e2abc72fcc9b66bb81807750a
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-10-17 19:11:32 -0300

    :white_check_mark: test: sign-in tests

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -35,3 +35,43 @@ describe("Testing POST /sign-up", () => {
     expect(result.status).toBe(422);
   });
 });
+
+describe("Testing POST /sign-in", () => {
+  it("Login with a correct body and return status code 200 and a token", async () => {
+    const body = await userFactory();
+    await supertest(app).post("/sign-up").send(body);
+    const user = { email: body.email, password: body.password };
+    const result = await supertest(app).post("/sign-in").send(user);
+    expect(result.status).toBe(200);
+    expect(result.body.token.length).toBeGreaterThan(0);
+  });
+
+  it("Login with a incorrect password and return status code 401", async () => {
+    const body = await userFactory();
+    await supertest(app).post("/sign-up").send(body);
+    const user = {
+      email: body.email,
+      password: body.password + "incorrect_pass",
+    };
+    const result = await supertest(app).post("/sign-in").send(user);
+    expect(result.status).toBe(401);
+  });
+
+  it("Login with a email without register and return status code 404", async () => {
+    const body = await userFactory();
+    const user = { email: body.email, password: body.password };
+    const result = await supertest(app).post("/sign-in").send(user);
+    expect(result.status).toBe(404);
+  });
+
+  it("Login with a incorrect body and return status code 422", async () => {
+    const body = await userFactory();
+    await supertest(app).post("/sign-up").send(body);
+    const user = {
+      email: "invalid_email.com",
+      password: body.password,
+    };
+    const result = await supertest(app).post("/sign-in").send(user);
+    expect(result.status).toBe(422);
+  });
+});
